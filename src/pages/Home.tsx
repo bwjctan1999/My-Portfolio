27f5cf1f7ca1c4815d2b436bf127e66c9944b923
bwjctan1999 <benzwaltertan@gmail.com>
2022-07-16 19:11:58 +0800

    setup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
-    <div className="p-8">
+    <div className="min-w-screen min-h-screen p-8 bg-[url('images\background.jpeg')] bg-cover">
       <div className="">
-        <h1 className="text-base">Hello there, I'm</h1>
-        <h1 className="text-6xl">BENZ TAN</h1>
-        <div className="w-48 h-1 bg-black" />
-        <h1 className="text-base">Web Developer</h1>
+        <h1 className="text-base text-white">Hello there, I'm</h1>
+        <h1 className="text-6xl text-white">BENZ TAN</h1>
+        <div className="w-48 h-1 bg-white" />
+        <h1 className="text-base text-white">Web Developer</h1>
       </div>
     </div>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,6 @@ module.exports = {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
+  theme: {},
   plugins: [],
 }
