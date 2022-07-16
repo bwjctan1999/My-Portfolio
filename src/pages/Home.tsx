e000ff458d17df107c4c35f3f46d514c1f830c6d
bwjctan1999 <benzwaltertan@gmail.com>
2022-07-16 18:50:40 +0800

    setup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="p-8">
+      <div className="">
+        <h1 className="text-base">Hello there, I'm</h1>
+        <h1 className="text-6xl">BENZ TAN</h1>
+        <div className="w-48 h-1 bg-black" />
+        <h1 className="text-base">Web Developer</h1>
+      </div>
+    </div>
+  );
+}
