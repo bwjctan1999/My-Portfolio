095f79fe494d4a2870f01300c80ec5388e656284
bwjctan1999 <benzwaltertan@gmail.com>
2022-08-05 05:54:48 +0800

    made kiki fly and follow mouse

---
+++ src/components/Kiki/Kiki.tsx
@@ -0,0 +1,6 @@
+import kiki from "../../assets/images/KikiSprite.png";
+import "./Kiki.css";
+
+export default function Kiki() {
+  return <div className="kiki bg-[url('assets\images\KikiSprite.png')]"></div>;
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from "react";
+import Kiki from "../components/Kiki/Kiki";
+
 export default function Home() {
+  const [coor, setCoor] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => {
+      setCoor({ x: e.clientX, y: e.clientY });
+    });
+  }, []);
+
   return (
-    <div className="min-w-screen min-h-screen p-8 bg-[url('images\background.jpeg')] bg-cover">
-      <div className="">
-        <h1 className="text-base text-white">Hello there, I'm</h1>
-        <h1 className="text-6xl text-white">BENZ TAN</h1>
-        <div className="w-48 h-1 bg-white" />
-        <h1 className="text-base text-white">Web Developer</h1>
+    <div className="w-screen h-screen p-16 bg-[url('assets\images\background.jpeg')] bg-cover flex flex-col justify-center">
+      <div className="flex justify-center gap-4">
+        <div className="flex flex-col text text-center w-1/2 gap-3 text-white">
+          <h1 className="text-base">Hello there, I'm</h1>
+          <h1 className="text-6xl">BENZ TAN</h1>
+          <div className="w-48 h-1 mx-auto bg-white" />
+          <h1 className="text-base">Web Developer</h1>
+        </div>
+        <div className="w-1/2">
+          <div
+            className="absolute top-0 left-0"
+            style={{
+              transform: `translate(${coor.x}px,${coor.y}px)`,
+              transition: "transform 0.5s linear",
+            }}
+          >
+            <Kiki />
+          </div>
+        </div>
       </div>
     </div>
   );
