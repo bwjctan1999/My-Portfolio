31a18e5446d92e51bbc797cac78aaaadec813b2f
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-11-02 20:04:45 +0800

    added technologies component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import AboutMe from "./pages/AboutMe";
 import Home from "./pages/Home";
+import Technologies from "./pages/Technologies";
 
 function App() {
   return (
     <div>
       <Home />
       <AboutMe />
+      <Technologies />
     </div>
   );
 }
